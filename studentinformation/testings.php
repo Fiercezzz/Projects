70b84417876177b953dccd1fd1180f1dcf582c5c
sleepi <107908140+Fiercezzz@users.noreply.github.com>
2024-04-17 13:40:42 +0800

    Add files via upload

--- studentinformation/testings.php
+++ studentinformation/testings.php
@@ -1,9 +1,18 @@
 <?php
-echo "Hello World";
 // echo = print
 
 $a = 2001;
 $b = "February";
 
-echo "my name is inigo montoya, I was born on $a, and my birth month is $b.";
+echo "My name is Inigo Montoya, I was born on $a, and my birth month is $b. <br><br>";
+
+
+$txt1 = "W";
+$txt2 = "schools.com";
+$c = 1;
+$d = 2;
+$e = $c + $d;
+
+echo $txt1.$e.$txt2; 
+
 ?>
