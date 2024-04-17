8548bc5b38fc410a5b8736fdd0362f61288c44a4
mangyantribe <masangkaymm20@gmail.com>
2024-04-17 14:17:34 +0800

    task one

---
+++ studentinformation/task_one.php
@@ -0,0 +1,12 @@
+<?php
+
+	// display this array using loop
+	$items = array("One", "Two", "Three", "Four", "Five");
+	
+
+
+	// display this array using loop
+	$items = array("1" => "One", "1" => "One", "1" => "One", "1" => "One","1" => "One",);
+
+
+?>
