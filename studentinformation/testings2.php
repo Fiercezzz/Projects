0679a586a8f11aad81d5e6cd5dd12e3d6cef382e
sleepi <107908140+Fiercezzz@users.noreply.github.com>
2024-04-17 14:00:08 +0800

    Add files via upload

---
+++ studentinformation/testings2.php
@@ -0,0 +1,18 @@
+<?php
+
+$work = "working!";
+$items = array("Mouse", "Laptop", "Phone");
+var_dump($items);
+
+echo "<br>";
+echo "<br>";
+
+echo "This is what I am using right now: <br>";
+var_dump($items[1]);
+echo "<br>It is $work";
+
+
+
+
+
+?>
